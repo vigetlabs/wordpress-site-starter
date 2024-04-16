10a61f05cb5e1b58d6a4aa035554d4360e578662
Brian DiChiara <brian.dichiara@viget.com>
2024-04-16 10:11:27 -0500

    CTA Dark Full Width Pattern

---
+++ wp-content/themes/wp-starter/patterns/cta-dark.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Title: CTA: Dark (Full Width)
+ * Slug: wp-starter/cta-dark
+ * Categories: cta
+ * Viewport width: 1400
+ */
+
+?>
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|30","bottom":"var:preset|spacing|30"}},"elements":{"link":{"color":{"text":"var:preset|color|white"}}}},"backgroundColor":"prototype-blue-1","textColor":"white","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull has-white-color has-prototype-blue-1-background-color has-text-color has-background has-link-color" style="padding-top:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--30)"><!-- wp:heading {"textAlign":"center"} -->
+    <h2 class="wp-block-heading has-text-align-center">Headline Goes Here</h2>
+    <!-- /wp:heading -->
+
+    <!-- wp:paragraph {"align":"center"} -->
+    <p class="has-text-align-center">Lorem ipsum dolor sit amet consectetur. Egestas turpis gravida vestibulum auctor lacinia mattis ultrices viverra placerat. Mauris risus vel mattis nulla mi odio lobortis. Donec dignissim convallis turpis sapien nisi.</p>
+    <!-- /wp:paragraph -->
+
+    <!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
+    <div class="wp-block-buttons"><!-- wp:button {"backgroundColor":"white","textColor":"prototype-blue-1","style":{"elements":{"link":{"color":{"text":"var:preset|color|prototype-blue-1"}}}}} -->
+        <div class="wp-block-button"><a class="wp-block-button__link has-prototype-blue-1-color has-white-background-color has-text-color has-background has-link-color wp-element-button">Button</a></div>
+        <!-- /wp:button --></div>
+    <!-- /wp:buttons --></div>
+<!-- /wp:group -->
