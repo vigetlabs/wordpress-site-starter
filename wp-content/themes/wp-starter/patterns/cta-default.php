653bb9bb73935503f5f1a3e00633d6457f5c01eb
Brian DiChiara <brian.dichiara@viget.com>
2024-04-16 06:47:41 -0500

    CTA Alignment/layout adjustments

--- wp-content/themes/wp-starter/patterns/cta-default.php
+++ wp-content/themes/wp-starter/patterns/cta-default.php
@@ -7,16 +7,16 @@
  */
 
 ?>
-<!-- wp:heading -->
-<h2 class="wp-block-heading">Headline Goes Here</h2>
+<!-- wp:heading {"textAlign":"center"} -->
+<h2 class="wp-block-heading has-text-align-center">Headline Goes Here</h2>
 <!-- /wp:heading -->
 
 <!-- wp:paragraph {"align":"center"} -->
 <p class="has-text-align-center">Body text goes here. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat</p>
 <!-- /wp:paragraph -->
 
-<!-- wp:buttons -->
+<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
 <div class="wp-block-buttons"><!-- wp:button {"backgroundColor":"purple"} -->
-	<div class="wp-block-button"><a class="wp-block-button__link has-purple-background-color has-background wp-element-button">Button</a></div>
-<!-- /wp:button --></div>
+    <div class="wp-block-button"><a class="wp-block-button__link has-purple-background-color has-background wp-element-button">Button</a></div>
+    <!-- /wp:button --></div>
 <!-- /wp:buttons -->
