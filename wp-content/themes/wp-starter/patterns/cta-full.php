e118f2967abe92c73ebd92018dfbec68b07befce
Brian DiChiara <brian.dichiara@viget.com>
2024-04-18 12:51:23 -0500

    [#8] Experiment with CTA Pattern

---
+++ wp-content/themes/wp-starter/patterns/cta-full.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Title: CTA: Full
+ * Slug: wp-starter/cta-full
+ * Categories: cta
+ * Viewport width: 1400
+ */
+
+?>
+<!-- wp:acf/cta {"name":"acf/cta","data":{},"mode":"preview","layout":{"type":"constrained"}} -->
+<!-- /wp:acf/cta -->
