e708d8b5530e1d1a7590130e750fa958deda103b
Brian DiChiara <brian.dichiara@viget.com>
2024-04-19 08:30:03 -0500

    [#8] One last example before branching off and finalizing this component.

---
+++ wp-content/themes/wp-starter/patterns/cta-body.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Title: CTA: Body
+ * Slug: wp-starter/cta-body
+ * Categories: cta
+ * Viewport width: 1400
+ */
+
+?>
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|30","bottom":"var:preset|spacing|30"}}},"backgroundColor":"prototype-blue-1","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull has-prototype-blue-1-background-color has-background" style="padding-top:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--30)"><!-- wp:acf/cta {"backgroundColor":"white","style":{"spacing":{"padding":{"top":"var:preset|spacing|10","bottom":"var:preset|spacing|10"}},"background":{"backgroundSize":"cover","backgroundPosition":"50% 50%"}},"name":"acf/cta","mode":"preview","layout":{"type":"constrained"}} -->
+
+    <!-- /wp:acf/cta --></div>
+<!-- /wp:group -->
