c0bb31f900caacd73bf14bdef16795a14d5b69d4
Nathan Schmidt <91974372+nathan-schmidt-viget@users.noreply.github.com>
2024-04-23 12:43:26 -0600

    [#8] Setting up base style for cta

--- wp-content/themes/wp-starter/blocks/cta/render.php
+++ wp-content/themes/wp-starter/blocks/cta/render.php
@@ -15,10 +15,12 @@ $block_template = [
 		],
 	],
 ];
-$inner = [
+$inner          = [
 	'template' => $block_template,
 ];
 ?>
-<section <?php block_attrs( $block ); ?>>
-	<?php inner_blocks( $inner ); ?>
+<section <?php block_attrs( $block, 'flex flex-col gap-24 mx-auto p-24 lg:p-64' ); ?>>
+	<div class="flex flex-col gap-8 flex-1">
+		<?php inner_blocks( $inner ); ?>
+	</div>
 </section>
