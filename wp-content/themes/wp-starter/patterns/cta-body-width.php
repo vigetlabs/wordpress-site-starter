3969ad6734e9cfdce052218a5012cdd4d01644c6
Brian DiChiara <brian.dichiara@viget.com>
2024-04-24 09:38:01 -0500

    [#8] Small Adjustments

--- wp-content/themes/wp-starter/patterns/cta-body-width.php
+++ wp-content/themes/wp-starter/patterns/cta-body-width.php
@@ -7,8 +7,8 @@
  */
 
 ?>
-<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|30","bottom":"var:preset|spacing|30"}}},"backgroundColor":"prototype-accent","layout":{"type":"constrained"}} -->
-<div class="wp-block-group alignfull has-prototype-accent-background-color has-background" style="padding-top:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--30)"><!-- wp:acf/cta {"align":"none","backgroundColor":"white","style":{"spacing":{"padding":{"top":"var:preset|spacing|10","bottom":"var:preset|spacing|10"}},"background":{"backgroundSize":"cover","backgroundPosition":"50% 50%"}},"name":"acf/cta","mode":"preview","layout":{"type":"constrained"}} -->
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|30","bottom":"var:preset|spacing|30"}}},"backgroundColor":"accent","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull has-accent-background-color has-background" style="padding-top:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--30)"><!-- wp:acf/cta {"align":"none","backgroundColor":"white","style":{"spacing":{"padding":{"top":"var:preset|spacing|10","bottom":"var:preset|spacing|10"}},"background":{"backgroundSize":"cover","backgroundPosition":"50% 50%"}},"name":"acf/cta","mode":"preview","layout":{"align":"none","type":"constrained"}} -->
 
 	<!-- /wp:acf/cta --></div>
 <!-- /wp:group -->
