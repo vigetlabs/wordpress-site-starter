5aa88cc9bd04fba6695afc8c3fa37b4dbaecc463
Brian DiChiara <brian.dichiara@viget.com>
2024-04-25 13:10:01 -0500

    [#32] Reverse Scripts

--- bin/composer-scripts/ProjectEvents/PostCreateProjectScript.php
+++ bin/composer-scripts/ProjectEvents/PostCreateProjectScript.php
@@ -14,46 +14,6 @@ use Viget\ComposerScripts\ComposerScript;
 class PostCreateProjectScript extends ComposerScript {
 
 	/**
-	 * @var array
-	 */
-	private static array $info = [];
-
-	/**
-	 * @var string
-	 */
-	private static string $default_project_slug = 'wordpress-site-starter';
-
-	/**
-	 * @var string
-	 */
-	private static string $default_project_name = 'WP Site Starter';
-
-	/**
-	 * @var string
-	 */
-	private static string $default_host_name = 'wpstarter';
-
-	/**
-	 * @var string
-	 */
-	private static string $default_theme_name = 'WP Starter';
-
-	/**
-	 * @var string
-	 */
-	private static string $default_theme_slug = 'wp-starter';
-
-	/**
-	 * @var string
-	 */
-	private static string $default_package_name = 'WPStarter';
-
-	/**
-	 * @var string
-	 */
-	private static string $default_function_prefix = 'wpstarter_';
-
-	/**
 	 * Perform actions within this file.
 	 *
 	 * @param Event $event
@@ -63,134 +23,34 @@ class PostCreateProjectScript extends ComposerScript {
 	public static function execute( Event $event ): void {
 		self::setEvent( $event );
 
-		if ( ! self::needsSetup() ) {
-			return;
-		}
-
-		// Gather project info.
-		self::getProjectInfo();
-
-		// Perform project string replacements
-		self::updateProjectFiles();
-
-		// Modify the description in the composer.json file.
-		self::updateComposerDescription();
+		self::replaceComposerFile();
+		self::removeThisScript();
 	}
 
 	/**
-	 * Check to see if we should run setup.
-	 *
-	 * @return bool
-	 */
-	public static function needsSetup(): bool {
-		$package = self::$event->getComposer()->getPackage()->getName();
-
-		if ( ! str_contains( $package, self::$default_project_slug ) ) {
-			return false;
-		}
-
-		return true;
-	}
-
-	/**
-	 * Gather project info.
+	 * Replace the composer.json file with the composer.json.dist file.
 	 *
 	 * @return void
 	 */
-	public static function getProjectInfo(): void {
-		$name = ! empty( self::$info['name'] ) ? self::$info['name'] : 'My Project';
+	public static function replaceComposerFile(): void {
+		$composer_file    = self::translatePath( 'composer.json' );
+		$replacement_file = self::translatePath( 'composer.json.dist' );
 
-		self::$info['name'] = self::ask( 'What is the name of your project?', $name );
-		self::$info['slug'] = self::slugify( self::$info['name'] );
-
-		self::$info['slug'] = self::ask( 'Do you want to use a custom project slug?', self::$info['slug'] );
-
-		$summary  = PHP_EOL . ' - Name: ' . self::$info['name'];
-		$summary .= PHP_EOL . ' - Slug: ' . self::$info['slug'];
-
-		self::writeOutput( '<info>Summary:</info>' . $summary );
-
-		if ( ! self::confirm( 'Does everything look good?' ) ) {
-			self::getProjectInfo();
-		}
-	}
-
-	/**
-	 * Slugify some text.
-	 *
-	 * @param string $text
-	 *
-	 * @return string
-	 */
-	private static function slugify( string $text ): string {
-		$separator = '-';
-
-		// replace non letter or digits by separator
-		$text = preg_replace( '~[^\pL\d]+~u', $separator, $text );
-
-		// transliterate
-		$text = iconv( 'utf-8', 'us-ascii//TRANSLIT', $text );
-
-		// remove unwanted characters
-		$text = preg_replace( '~[^-\w]+~', '', $text );
-
-		// trim
-		$text = trim( $text, $separator );
-
-		// remove duplicate separator
-		$text = preg_replace( '~-+~', $separator, $text );
-
-		// lowercase
-		$text = strtolower( $text );
-
-		if ( empty( $text ) ) {
-			return '';
-		}
-
-		return $text;
-	}
-
-	/**
-	 * Change wordpress-starter-project to match new project
-	 *
-	 * @return void
-	 */
-	public static function updateProjectFiles(): void {
-		if ( empty( self::$info['slug'] ) ) {
-			self::writeError( 'Missing project slug.' );
+		if ( ! file_exists( $replacement_file ) ) {
 			return;
 		}
 
-		$files = [
-			'.ddev/config.yml',
-			'composer.json',
-			'phpcs.xml',
-		];
-
-		$search  = self::$default_project_slug;
-		$replace = self::$info['slug'];
-
-		foreach ( $files as $file ) {
-			self::searchReplaceFile( $search, $replace, $file );
-		}
-
-		self::writeInfo( 'All set!' );
+		copy( $replacement_file, $composer_file );
+		unlink( $replacement_file );
 	}
 
 	/**
-	 * Modify the composer.json project description
+	 * Self destruct.
 	 *
 	 * @return void
 	 */
-	public static function updateComposerDescription() {
-		if ( empty( self::$info['name'] ) ) {
-			self::writeError( 'Missing project name.' );
-			return;
-		}
-
-		$search  = self::$event->getComposer()->getPackage()->getDescription();
-		$replace = sprintf( 'A custom WordPress Site for %s by Viget.', self::$info['name'] );
-
-		self::searchReplaceFile( $search, $replace, 'composer.json' );
+	public static function removeThisScript(): void {
+		$script_file = self::translatePath( __FILE__ );
+		unlink( $script_file );
 	}
 }
