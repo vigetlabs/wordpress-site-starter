7f02e2c8fb9a67492131240787f6df2397f17dbd
Brian DiChiara <brian.dichiara@viget.com>
2024-04-26 09:13:43 -0500

    [#13] PR Feedback

--- wp-content/themes/wp-starter/blocks/alert-banner/block.php
+++ wp-content/themes/wp-starter/blocks/alert-banner/block.php
@@ -8,17 +8,18 @@
 /**
  * Alert Banner Dismiss Button
  *
+ * @param string $id Alert Banner ID
+ *
  * @return void
  */
-function alert_banner_dismiss_button($id = 'alert-banner'): void {
+function alert_banner_dismiss_button( string $id ): void {
 	$dismiss_button = sprintf(
-		'<button class="dismiss" aria-label="%s" title="%s" @click="%s = !%s">',
+		'<button class="dismiss" aria-label="%1$s" title="%2$s" @click="%3$s = !%3$s">',
 		esc_attr__( 'Dismiss alert banner', 'wp-starter' ),
 		esc_attr__( 'Dismiss', 'wp-starter' ),
-		$id,
-		$id
+		esc_attr( $id )
 	);
 	$dismiss_button .= '</button>';
 
-	echo ( $dismiss_button );
+	echo $dismiss_button;
 }
