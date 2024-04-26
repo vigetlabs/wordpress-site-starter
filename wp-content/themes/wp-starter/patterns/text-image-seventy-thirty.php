3917c202fef170b2eb5f0b44b1bacb4ec90465c1
Brian DiChiara <brian.dichiara@viget.com>
2024-04-26 09:55:16 -0500

    [#7] Fix Text & Image Patterns

--- wp-content/themes/wp-starter/patterns/text-image-seventy-thirty.php
+++ wp-content/themes/wp-starter/patterns/text-image-seventy-thirty.php
@@ -8,9 +8,9 @@
 
 ?>
 <!-- wp:acf/text-image {"name":"acf/text-image","data":{},"mode":"preview"} -->
-<!-- wp:media-text {"mediaWidth":70} -->
-<div class="wp-block-media-text is-stacked-on-mobile" style="grid-template-columns:60% auto"><figure class="wp-block-media-text__media"></figure><div class="wp-block-media-text__content"><?php
-render_block_core_pattern( [ 'slug' => 'wp-starter/text-image-inner-blocks' ] );
+<!-- wp:media-text {"imageFill":true,"mediaWidth":70,"verticalAlignment":"center"} -->
+<div class="wp-block-media-text is-stacked-on-mobile is-vertically-aligned-center is-image-fill" style="grid-template-columns:70% auto"><figure class="wp-block-media-text__media"></figure><div class="wp-block-media-text__content"><?php
+	require get_stylesheet_directory() . '/patterns/text-image-media-content.php';
 ?></div></div>
 <!-- /wp:media-text -->
 <!-- /wp:acf/text-image -->
