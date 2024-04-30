c9045c6dcaf76269bb84cc8afff68af35d880b2a
Brian DiChiara <brian.dichiara@viget.com>
2024-04-30 12:08:10 -0500

    [N/A] Update Text/Image Pattern References, Restore lost file

--- wp-content/themes/wp-starter/blocks/text-image/patterns/text-image-inner-blocks.php
+++ wp-content/themes/wp-starter/blocks/text-image/patterns/text-image-inner-blocks.php
@@ -7,9 +7,10 @@
  * Inserter: no
  */
 
+$pattern_dir = dirname( __FILE__ );
 ?>
 <!-- wp:media-text {"imageFill":true,"verticalAlignment":"center"} -->
 <div class="wp-block-media-text is-stacked-on-mobile is-vertically-aligned-center is-image-fill"><figure class="wp-block-media-text__media"></figure><div class="wp-block-media-text__content"><?php
-	require get_stylesheet_directory() . '/patterns/text-image-media-content.php';
+	require $pattern_dir . '/text-image-media-content.php';
 ?></div></div>
 <!-- /wp:media-text -->
--- wp-content/themes/wp-starter/blocks/text-image/patterns/text-image-sixty-forty.php
+++ wp-content/themes/wp-starter/blocks/text-image/patterns/text-image-sixty-forty.php
@@ -6,11 +6,12 @@
  * Viewport width: 1400
  */
 
+$pattern_dir = dirname( __FILE__ );
 ?>
 <!-- wp:acf/text-image {"name":"acf/text-image","data":{},"mode":"preview"} -->
 <!-- wp:media-text {"imageFill":true,"mediaWidth":60,"verticalAlignment":"center"} -->
 <div class="wp-block-media-text is-stacked-on-mobile is-vertically-aligned-center is-image-fill" style="grid-template-columns:60% auto"><figure class="wp-block-media-text__media"></figure><div class="wp-block-media-text__content"><?php
-	require get_stylesheet_directory() . '/patterns/text-image-media-content.php';
+	require $pattern_dir . '/text-image-media-content.php';
 ?></div></div>
 <!-- /wp:media-text -->
 <!-- /wp:acf/text-image -->
