3dbb4345bcd7a49dafb79d746767c7f4d11469a4
Brian DiChiara <122309362+bd-viget@users.noreply.github.com>
2024-05-17 13:08:52 -0500

    [#58] Remove Core Blocks (#76)
    * [#58] Disable specific blocks.
    
    * [N/A] Update ACF
    
    * [#58] Fixed bug with block removal, sorted blocks
    
    * [#58] Included Classic, Cover & Gallery blocks per ticket discussion

--- wp-content/themes/wp-starter/inc/blocks.php
+++ wp-content/themes/wp-starter/inc/blocks.php
@@ -19,3 +19,33 @@ add_filter(
 		return $categories;
 	}
 );
+
+// Disable some of the default core blocks.
+add_filter(
+	'allowed_block_types_all',
+	function ( array|bool $allowed_block_types, WP_Block_Editor_Context $context ): array|bool {
+		// TODO: Maybe filter by $context.
+
+		if ( ! is_array( $allowed_block_types ) ) {
+			$allowed_block_types = array_keys( WP_Block_Type_Registry::get_instance()->get_all_registered() );
+		}
+
+		$removed_blocks = [
+			'core/archives',
+			'core/audio',
+			'core/calendar',
+			'core/html',
+			'core/latest-comments',
+			'core/latest-posts',
+			'core/more',
+			'core/nextpage',
+			'core/preformatted',
+			'core/rss',
+			'core/verse',
+		];
+
+		return array_values( array_diff( $allowed_block_types, $removed_blocks ) );
+	},
+	10,
+	2
+);
