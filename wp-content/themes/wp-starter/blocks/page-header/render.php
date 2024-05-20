8661ef96462988b6f1ce10c18997ffe681551624
Brian DiChiara <brian.dichiara@viget.com>
2024-05-20 17:09:51 -0500

    [#69] Page Header Component

---
+++ wp-content/themes/wp-starter/blocks/page-header/render.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Block: Page Header
+ *
+ * @global array $block
+ *
+ * @package WPStarter
+ */
+
+$block_template = [
+	[
+		'core/pattern',
+		[
+			'slug' => 'wp-starter/page-header-inner-blocks',
+		],
+	],
+];
+$inner          = [
+	'template' => $block_template,
+];
+?>
+<section <?php block_attrs( $block ); ?>>
+	<?php inner_blocks( $inner ); ?>
+</section>
