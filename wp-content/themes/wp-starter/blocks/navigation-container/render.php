e64e3ca53fe5343ab87ae3b3f587b36a2dac738f
Nathan Schmidt <91974372+nathan-schmidt-viget@users.noreply.github.com>
2024-05-20 15:55:41 -0600

    [#67] Fixing mobile layout erros

--- wp-content/themes/wp-starter/blocks/navigation-container/render.php
+++ wp-content/themes/wp-starter/blocks/navigation-container/render.php
@@ -7,34 +7,41 @@
  * @package WPStarter
  */
 
-$behavior = get_field( 'mobile_behavior' ) ?: 'collapse';
-$state    = get_field( 'default_state' ) ?: 'visible';
-$attrs    = [];
+$attrs = [];
 
 ?>
-<div <?php block_attrs( $block, 'wp-block-group', $attrs ); ?>
-	x-data="dropdown"
-	x-on:keydown.escape.prevent.stop="close($refs.button)"
-	x-on:focusin.window="! $refs.panel.contains($event.target) && close()"
-	x-id="['dropdown-button']"
+<div <?php block_attrs( $block, 'wp-block-group navigation-container flex flex-col items-end gap-5 lg:w-auto w-full', $attrs ); ?>
+	x-data="{menuIsOpen : false}"
+	x-trap="menuIsOpen"
 >
 	<button
-		class="navigation-toggle p-1"
+		@click="menuIsOpen = !menuIsOpen"
+		x-cloak
+		class="lg:hidden absolute top-0"
 		aria-rel="mobile-toggle"
 		aria-label="<?php esc_attr_e( 'Toggle menu', 'wp-starter' ); ?>"
 	>
-		<svg xmlns="http://www.w3.org/2000/svg" width="27" height="18" viewBox="0 0 27 18" fill="currentColor" class="menu-open">
+		<svg class="m-2" x-show="!menuIsOpen" xmlns="http://www.w3.org/2000/svg" width="27" height="18" viewBox="0 0 27 18" fill="currentColor" class="menu-open">
 			<path d="M26.8059 1H0.805908" stroke="currentColor" stroke-width="1.5"></path>
 			<path d="M26.8059 17H0.805908" stroke="currentColor" stroke-width="1.5"></path>
 			<path d="M26.8059 9H0.805908" stroke="currentColor" stroke-width="1.5"></path>
 		</svg>
-		<svg xmlns="http://www.w3.org/2000/svg" width="21" height="20" fill="currentColor" class="menu-close">
+		<svg class="m-2" x-show="menuIsOpen" xmlns="http://www.w3.org/2000/svg" width="21" height="20" fill="currentColor" class="menu-close">
 			<path d="M19.192 19.192.808.808M1 19.192 19.385.808" stroke="currentColor" stroke-width="2"></path>
 		</svg>
 	</button>
 
-	<div class="wp-block-group navigation-content"
+	<div class="wp-block-group navigation-content lg:!block w-full lg:w-auto"
+		x-show="menuIsOpen"
+		@click.away="menuIsOpen = false"
 	>
-		<?php inner_blocks(); ?>
+		<div
+			x-data="dropdown"
+			x-on:keydown.escape.prevent.stop="close($refs.button)"
+			x-on:focusin.window="! $refs.panel.contains($event.target) && close()"
+			x-id="['dropdown-button']"
+		>
+			<?php inner_blocks(); ?>
+		</div>
 	</div>
 </div>
