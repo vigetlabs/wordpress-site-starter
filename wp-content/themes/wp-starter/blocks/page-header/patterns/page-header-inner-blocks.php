8afea794453ffe49af0cfaf9a61b29cde57f712d
Nathan Schmidt <91974372+nathan-schmidt-viget@users.noreply.github.com>
2024-05-21 14:42:15 -0600

    [#69] Styling the breadcrumbs

--- wp-content/themes/wp-starter/blocks/page-header/patterns/page-header-inner-blocks.php
+++ wp-content/themes/wp-starter/blocks/page-header/patterns/page-header-inner-blocks.php
@@ -11,17 +11,22 @@
 <!-- wp:acf/breadcrumbs {"name":"acf/breadcrumbs","data":{},"mode":"preview"} /-->
 
 <!-- wp:columns {"verticalAlignment":"center","style":{"spacing":{"margin":{"top":"0","bottom":"0"}}}} -->
-<div class="wp-block-columns are-vertically-aligned-center" style="margin-top:0;margin-bottom:0"><!-- wp:column {"verticalAlignment":"center","width":"66.66%"} -->
-	<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:66.66%"><!-- wp:post-title {"level":1} /-->
+<div class="wp-block-columns are-vertically-aligned-center" style="margin-top:0;margin-bottom:0">
+<!-- wp:column {"verticalAlignment":"center","width":"66.66%"} -->
+	<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:66.66%">
+		<!-- wp:post-title {"level":1,"style":{"spacing":{"padding":{"top":"var:preset|spacing|20","bottom":"var:preset|spacing|20"}}}} /-->
 
 		<!-- wp:paragraph -->
 		<p>Page description ponam in culpa idiota aliis pravitatis. Principium ponere culpam in se justum praeceptum. Neque improperes et aliis qui non perfecte ipse docuit.</p>
-		<!-- /wp:paragraph --></div>
+		<!-- /wp:paragraph -->
+	</div>
 	<!-- /wp:column -->
 
 	<!-- wp:column {"verticalAlignment":"center","width":"33.33%"} -->
 	<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:33.33%"><!-- wp:image -->
 		<figure class="wp-block-image"><img alt=""/></figure>
-		<!-- /wp:image --></div>
-	<!-- /wp:column --></div>
+		<!-- /wp:image -->
+	</div>
+	<!-- /wp:column -->
+</div>
 <!-- /wp:columns -->
