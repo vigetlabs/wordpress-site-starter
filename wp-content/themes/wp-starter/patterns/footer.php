ef3a97b50652cd5e326323dac4f423b1f632f177
Brian DiChiara <122309362+bd-viget@users.noreply.github.com>
2024-05-23 16:44:18 -0500

    [#68] Footer Navigation and Navigation Container Tweaks (#87)
    * [#68] Footer Navigation and Navigation Container Tweaks
    
    * [#68] Add default background color to new pattern

--- wp-content/themes/wp-starter/patterns/footer.php
+++ wp-content/themes/wp-starter/patterns/footer.php
@@ -1,12 +1,10 @@
 <?php
 /**
- * Title: Footer with colophon
+ * Title: Footer with Navigation
  * Slug: wp-starter/footer
  * Categories: footer
  * Block Types: core/template-part/footer
  */
 
 ?>
-<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"}}},"layout":{"type":"constrained"}} -->
-<div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50)"><!-- wp:site-title {"level":0,"fontSize":"medium"} /--></div>
-<!-- /wp:group -->
+<!-- wp:template-part {"slug":"footer"} /-->
