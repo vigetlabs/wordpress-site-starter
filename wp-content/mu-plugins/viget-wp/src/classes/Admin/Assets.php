291c39ba4c3bedb68b1cfc1b3a65dc82bdf42f84
Brian DiChiara <brian.dichiara@viget.com>
2024-06-21 06:51:28 -0500

    [#114] Allow Skip Cropping

--- wp-content/mu-plugins/viget-wp/src/classes/Admin/Assets.php
+++ wp-content/mu-plugins/viget-wp/src/classes/Admin/Assets.php
@@ -28,11 +28,33 @@ class Assets {
 		add_action(
 			'admin_enqueue_scripts',
 			function () {
+				$version = vigetwp()->get_version();
+
+				if ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ) {
+					$js_path = VIGETWP_PLUGIN_PATH . 'src/assets/js/skip-cropping.js';
+					$version = filemtime( $js_path );
+				}
+
+				wp_enqueue_script(
+					'vigetwp-admin-skip-cropping',
+					VIGETWP_PLUGIN_URL . 'src/assets/js/skip-cropping.js',
+					[ 'jquery', 'media-views' ],
+					$version,
+					[
+						'in_footer' => true,
+					]
+				);
+
+				if ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ) {
+					$css_path = VIGETWP_PLUGIN_PATH . 'src/assets/css/admin.css';
+					$version  = filemtime( $css_path );
+				}
+
 				wp_enqueue_style(
 					'vigetwp-admin-styles',
 					VIGETWP_PLUGIN_URL . 'src/assets/css/admin.css',
 					[], // dependencies.
-					vigetwp()->get_version()
+					$version
 				);
 			}
 		);
