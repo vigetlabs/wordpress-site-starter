e0584bce10e3370aefee70aa1fbb92a9b3dbaad9
Brian DiChiara <brian.dichiara@viget.com>
2024-07-01 17:49:01 -0500

    [N/A] ACF Form Block Proof of concept

---
+++ wp-content/plugins/acf-form-blocks/blocks/submit/block.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Block: Submit
+ *
+ * @package ACFFormBlocks
+ */
+
+add_filter(
+	'render_block',
+	function ( string $block_content, array $block ) {
+		if ( 'acf/submit' !== $block['blockName'] ) {
+			return $block_content;
+		}
+
+		// convert link tag to a button tag and add type submit
+		$block_content = str_replace( '<a', '<button type="submit"', $block_content );
+		return str_replace( '</a>', '</button>', $block_content );
+	},
+	10,
+	2
+);
