3d98ed7f9c304d4dbf506328d811b1249f6f1865
Brian DiChiara <brian.dichiara@viget.com>
2024-07-02 08:02:12 -0500

    [N/A] Retain input values

--- wp-content/plugins/acf-form-blocks/blocks/textarea/render.php
+++ wp-content/plugins/acf-form-blocks/blocks/textarea/render.php
@@ -8,9 +8,10 @@
  */
 
 use ACFFormBlocks\Block;
+use ACFFormBlocks\Field;
 use ACFFormBlocks\Template;
 
-$required = get_field( 'required' );
+$field = new Field( $block );
 
 $inner = [
 	'template' => ( new Template( new Block( 'core/paragraph', [ 'placeholder' => __( 'Field Label...', 'acf-form-blocks' ) ] ) ) )->get(),
@@ -23,9 +24,12 @@ $inner = [
 		endif;
 	?>>
 		<?php inner_blocks( $inner ); ?>
-		<?php if ( $required ) : ?>
+		<?php if ( $field->is_required() ) : ?>
 			<span class="is-required">*</span>
 		<?php endif; ?>
 	</label>
-	<textarea name="<?php echo esc_attr( $block['id'] ); ?>" <?php block_attrs( $block ); ?>></textarea>
+	<textarea
+		name="<?php echo esc_attr( $field->get_name() ); ?>"
+		<?php block_attrs( $block ); ?>
+	><?php echo esc_textarea( $field->get_value() ); ?></textarea>
 </div>
