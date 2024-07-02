b5c17905a4c3f84a2bb11cf1c20a923747309b8b
Brian DiChiara <brian.dichiara@viget.com>
2024-07-02 18:00:13 -0500

    [N/A] Prototype Select Field

---
+++ wp-content/plugins/acf-form-blocks/blocks/select/block.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Input block
+ *
+ * @package ACFFormBlocks
+ */
+
+use ACFFormBlocks\Field;
+
+add_filter(
+	'acfbt_block_attrs',
+	function ( array $attrs, array $block ): array {
+		if ( 'acf/select' !== $block['name'] ) {
+			return $attrs;
+		}
+
+		$field = new Field( $block );
+
+		$attrs['name'] = $field->get_name();
+
+		if ( $field->get_placeholder() ) {
+			$attrs['placeholder'] = $field->get_placeholder();
+		}
+
+		if ( $field->is_required() ) {
+			$attrs['required'] = true;
+		}
+
+		return $attrs;
+	},
+	10,
+	2
+);
