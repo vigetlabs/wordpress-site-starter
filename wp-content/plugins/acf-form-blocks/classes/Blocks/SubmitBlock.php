dd727e1dad1d94a0c148a77e3d3c7017aa114417
Brian DiChiara <brian.dichiara@viget.com>
2024-07-09 10:29:49 -0500

    [N/A] Reorganization due to namespacing

---
+++ wp-content/plugins/acf-form-blocks/classes/Blocks/SubmitBlock.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Submit Block Helper Class
+ *
+ * @package ACFFormBlocks
+ */
+
+namespace ACFFormBlocks\Blocks;
+
+/**
+ * Submit Block Class
+ */
+class SubmitBlock extends FieldBlock {
+
+	/**
+	 * Convert link tag to a button tag and add type submit
+	 *
+	 * @param string $block_content
+	 *
+	 * @return string
+	 */
+	public function render( string $block_content ): string {
+		// convert link tag to a button tag and add type submit
+		$block_content = str_replace( '<a', '<button type="submit"', $block_content );
+
+		return str_replace( '</a>', '</button>', $block_content );
+	}
+}
