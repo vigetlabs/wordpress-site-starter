0c9d554ce259843d69b81d05b10ee46dca3ebae7
Brian DiChiara <brian.dichiara@viget.com>
2024-07-16 19:00:01 -0500

    [N/A] New input fields!!

---
+++ wp-content/plugins/acf-form-blocks/classes/Elements/Color.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Color Input Field Class
+ *
+ * @package ACFFormBlocks
+ */
+
+namespace ACFFormBlocks\Elements;
+
+/**
+ * Class for Color Input Fields
+ */
+class Color extends Input {
+
+	/**
+	 * Get the default value
+	 *
+	 * @return mixed
+	 */
+	public mixed $default_value = '#000000';
+
+}
