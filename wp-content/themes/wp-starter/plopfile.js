2444cc592b957f4b473b8d959580631d5bcf68de
Nathan Schmidt <91974372+nathan-schmidt-viget@users.noreply.github.com>
2024-07-24 10:50:51 -0600

    [#129] Adding styles and variations

--- wp-content/themes/wp-starter/plopfile.js
+++ wp-content/themes/wp-starter/plopfile.js
@@ -6,14 +6,68 @@ module.exports = function (plop) {
 			{
 				type: 'input',
 				name: 'name',
-				message: 'What is the block name?'
+				message: 'What is the block name?',
+				validate: function (value) {
+					if (/.+/.test(value)) {
+					  return true;
+					}
+					return "Name is required";
+				  },
 			},
+			{
+				type: 'confirm',
+				name: 'styles',
+				message: 'Do you want block styles?'
+			},
+			{
+				type: 'confirm',
+				name: 'variations',
+				message: 'Do you want block variations?'
+			}
 		],
-		actions: [{
-			type: 'addMany',
-			destination: 'blocks/{{name}}/',
-			base: 'plop-templates',
-			templateFiles: 'plop-templates/*.hbs'
-		}]
+		actions: function(data) {
+			var actions = [{
+				type: 'addMany',
+				destination: 'blocks/{{dashCase name}}/',
+				base: 'plop-templates',
+				templateFiles: 'plop-templates/*.hbs',
+				abortOnFail: true,
+			}];
+
+			// add styles
+			if (data.styles) {
+				actions.push({
+					type: 'modify',
+					path: 'blocks/{{dashCase name}}/block.json',
+					pattern: /-- PREPEND STYLES HERE --/gi,
+					templateFile: 'plop-templates/parts/styles.json.hbs'
+				});
+			} else {
+				actions.push({
+					type: 'modify',
+					path: 'blocks/{{dashCase name}}/block.json',
+					pattern: /-- PREPEND STYLES HERE --/gi
+				});
+			}
+
+			// add styles
+			if (data.styles) {
+				actions.push({
+					type: 'modify',
+					path: 'blocks/{{dashCase name}}/block.json',
+					pattern: /-- PREPEND VARIATIONS HERE --/gi,
+					templateFile: 'plop-templates/parts/variations.json.hbs'
+				});
+			} else {
+				actions.push({
+					type: 'modify',
+					path: 'blocks/{{dashCase name}}/block.json',
+					pattern: /-- PREPEND VARIATIONS HERE --/gi
+				});
+			}
+
+
+            return actions;
+        }
 	});
 };
