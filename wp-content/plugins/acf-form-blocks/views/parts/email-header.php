2ee6f4abd1991a9c5c5ffc34d8effd772d5e8372
Brian DiChiara <brian.dichiara@viget.com>
2024-07-26 11:49:30 -0500

    [N/A] Basic Email Template styling

--- wp-content/plugins/acf-form-blocks/views/parts/email-header.php
+++ wp-content/plugins/acf-form-blocks/views/parts/email-header.php
@@ -12,11 +12,12 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title><?php bloginfo( 'name' ); ?></title>
+	<?php do_action( 'acffb_email_head' ); ?>
 </head>
-<body>
-<table width="100%" cellpadding="0" cellspacing="0" border="0">
-	<tr>
-		<td align="center">
-			<table width="600" cellpadding="0" cellspacing="0" border="0">
-				<tr>
-					<td>
+<body class="acffb-email-body">
+	<table width="100%" cellpadding="0" cellspacing="0" border="0">
+		<tr>
+			<td align="center">
+				<table width="600" cellpadding="0" cellspacing="0" border="0">
+					<tr>
+						<td>
