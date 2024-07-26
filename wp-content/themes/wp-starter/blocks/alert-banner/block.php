17fda3f9d3a7049d01c614c6b6967937641a8d45
Nathan Schmidt <91974372+nathan-schmidt-viget@users.noreply.github.com>
2024-07-26 16:40:24 -0600

    [#127] Updating alert banner

--- wp-content/themes/wp-starter/blocks/alert-banner/block.php
+++ wp-content/themes/wp-starter/blocks/alert-banner/block.php
@@ -26,7 +26,7 @@ add_action(
  * @return void
  */
 function alert_banner_dismiss_button( string $id ): void {
-	$dismiss_button = sprintf(
+	$dismiss_button  = sprintf(
 		'<button class="dismiss" aria-label="%1$s" title="%2$s" @click="%3$s = !%3$s">',
 		esc_attr__( 'Dismiss alert banner', 'wp-starter' ),
 		esc_attr__( 'Dismiss', 'wp-starter' ),
--- wp-content/themes/wp-starter/blocks/alert-banner/render.php
+++ wp-content/themes/wp-starter/blocks/alert-banner/render.php
@@ -7,12 +7,19 @@
  * @package WPStarter
  */
 
+$id             = 'alert' . $block['block_id'] ?? $block['id'];
 $block_template = [
 	[
 		'core/paragraph',
-		[
-			'placeholder' => __( 'Enter alert banner message...', 'wp-starter' ),
-		],
+		{
+			'placeholder': 'Enter alert banner message...'
+		},
+	],
+	[
+		'core/button',
+		{
+			'className': 'is-style-outline',
+		},
 	],
 ];
 $allowed        = [
@@ -23,19 +30,16 @@ $inner          = [
 	'template'      => $block_template,
 	'allowedBlocks' => $allowed,
 ];
-
-$attrs = [];
+$attrs          = [];
 
 if ( ! is_admin() ) {
-	$attrs['x-data'] = '{ ' . $block['id'] . ': $persist(true) }';
-	$attrs['x-show'] = $block['id'];
+	$attrs['x-data'] = '{ ' . $id . ': $persist(true) }';
+	$attrs['x-show'] = $id;
 }
 ?>
-<section
- 	<?php block_attrs( $block, "gap-24 lg:gap-48", $attrs ); ?>
- >
-	<div class="flex flex-col items-start gap-24 lg:flex-row lg:gap-48 lg:items-center">
+<section <?php block_attrs( $block, '', $attrs ); ?> >
+	<div class="wp-block-alert-banner__inner-container">
 		<?php inner_blocks( $inner ); ?>
 	</div>
-	<?php alert_banner_dismiss_button( $block['id'] ); ?>
+	<?php alert_banner_dismiss_button( $id ); ?>
 </section>
