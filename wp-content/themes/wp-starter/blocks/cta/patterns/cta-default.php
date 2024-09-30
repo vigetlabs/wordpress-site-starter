b20bfbfcd90a33afd0e065136701c3ed700fccb2
Brian DiChiara <brian.dichiara@viget.com>
2024-09-30 10:31:55 -0500

    [N/A] Restore CTA & Image/Text Patterns

---
+++ wp-content/themes/wp-starter/blocks/cta/patterns/cta-default.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Title: Call to Action (Full Width)
+ * Slug: wp-starter/cta-default
+ * Categories: cta
+ * Viewport width: 1400
+ */
+
+?>
+<!-- wp:acf/cta {"name":"acf/cta","data":{},"mode":"preview","layout":{"type":"constrained"}} -->
+<!-- /wp:acf/cta -->
