fc2e128c7139c206977e7be5ef3f406d04f06d99
Brian DiChiara <brian.dichiara@viget.com>
2024-10-11 10:55:00 -0500

    [#149] Video Player Component

---
+++ wp-content/themes/wp-starter/blocks/video-player/block.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Video Player Block
+ *
+ * @package WPStarter
+ */
+
+add_action(
+	'after_setup_theme',
+	function () {
+		add_theme_support( 'responsive-embeds' );
+	}
+);
+
+// Enable the YT JSON API
+add_filter(
+	'embed_oembed_html',
+	function ( string $code ): string {
+		return preg_replace(
+			'@embed/([^"&]*)@',
+			'embed/$1?si=BSrdaVGKXA6hb2GO&enablejsapi=1&html5=1&mute=0',
+			$code
+		);
+	}
+);
+
+// Register Play Button Block Style
+add_action(
+	'init',
+	function () {
+		register_block_style(
+			'core/button',
+			[
+				'name'  => 'play-video',
+				'label' => __( 'Play Video', 'wp-starter' ),
+			]
+		);
+	}
+);
+
+add_filter(
+	'acfbt_button_icons',
+	function ( array $icons ): array {
+		$block_dir       = dirname( __FILE__ );
+		$play_icon_path = $block_dir . '/images/video-play-button.svg';
+
+		return array_merge([
+			'video-play-button' => [
+				'label'       => __( 'Video Play Button', 'wp-starter' ),
+				'icon'        => file_get_contents( $play_icon_path ),
+				'defaultLeft' => false,
+			],
+		], $icons );
+	},
+	9
+);
---
+++ wp-content/themes/wp-starter/src/components/playvideo.js
@@ -0,0 +1,34 @@
+document.addEventListener('alpine:init', () => {
+	YouTubePlayer();
+})
+
+function YouTubePlayer() {
+	//Get the video cover and play button
+	const videoCover = document.querySelectorAll(".acf-block-video-player .wp-block-cover");
+	const playButton = document.querySelectorAll(".acf-block-video-player .is-style-play-video > a");
+	const iframe = document.querySelectorAll('.acf-block-video-player iframe');
+
+	//Add Alpine to the HTML
+	videoCover.forEach(element => {
+		element.setAttribute('x-show', '!playvideo');
+	});
+
+	//Set the video ref
+	iframe.forEach(element => {
+		element.setAttribute('x-ref', 'video');
+	});
+
+	//Set the event listener for the play
+	playButton.forEach(element => {
+		element.setAttribute('x-on:click', 'play()');
+		element.setAttribute('href', '#playvideo');
+	});
+}
+
+export default () => ({
+	playvideo: false,
+	play() {
+		this.playvideo = true;
+		this.$refs.video.contentWindow.postMessage('{"event":"command","func":"playVideo","args":""}', '*');
+	}
+});
