07959992c2e025c564637202e2ed0b7c407722a8
Brian DiChiara <brian.dichiara@viget.com>
2024-10-11 10:57:52 -0500

    [N/A] Update Composer Script

--- bin/composer-scripts/ProjectEvents/PostInstallScript.php
+++ bin/composer-scripts/ProjectEvents/PostInstallScript.php
@@ -62,7 +62,7 @@ class PostInstallScript extends ComposerScript {
 			],
 		],
 		'seo-by-rank-math' => 'Rank Math SEO',
-		'svg-support' => 'SVG Support',
+		'safe-svg' => 'Safe SVG',
 		'viget-parts-kit' => 'Viget Parts Kit',
 		'wordfence' => 'Wordfence',
 	];
