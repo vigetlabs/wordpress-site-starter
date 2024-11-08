3d38fd5626f2289a95d59f1778af8ef348e6d4a9
Nathan Schmidt <91974372+nathan-schmidt-viget@users.noreply.github.com>
2024-11-08 08:58:27 -0700

    [#148] Moving JS over to Alpine

--- wp-content/themes/wp-starter/blocks/dialog/render.php
+++ wp-content/themes/wp-starter/blocks/dialog/render.php
@@ -14,9 +14,41 @@ $cbx_id = uniqid();
 	<label for="<?php echo esc_attr( $cbx_id ); ?>" class="acfbt-dialog-label"><?php esc_html_e( 'Toggle', 'wp-site-starter' ); ?></label>
 <?php endif; ?>
 
-<dialog <?php block_attrs( $block ); ?>>
+<div
+<?php if ( ! is_admin() ) : ?>
+	x-data="{
+	openDialog: false,
+
+	// Close the dialog when the user clicks backdrop
+	handleDialogClick(event) {
+		(event.target === $refs.dialogRef) && this.handleDialogClose()
+	},
+
+	// Delay close to allow for animation
+	handleDialogClose() {
+		if (!this.openDialog) return
+
+		this.openDialog = false
+		$refs.dialogRef.close()
+	}
+	}"
+<?php endif; ?>
+>
+
+<dialog
+	<?php block_attrs( $block ); ?>
 	<?php if ( ! is_admin() ) : ?>
-<!--		<button>--><?php //esc_html_e( 'Close', 'wp-site-starter' ); ?><!--</button>-->
+		x-ref="dialogRef"
+		@keydown.escape.prevent="handleDialogClose()"
+		@click="handleDialogClick(event)"
+	<?php endif; ?>
+>
+	<?php if ( ! is_admin() ) : ?>
+		<button
+			@click="handleDialogClose()"
+		>
+			<?php esc_html_e( 'Close', 'wp-site-starter' ); ?>
+		</button>
 	<?php endif; ?>
 
 	<div class="inner">
@@ -26,5 +58,12 @@ $cbx_id = uniqid();
 		<?php inner_blocks(); ?>
 	</div>
 </dialog>
-
-<button><?php esc_html_e( 'Open', 'wp-site-starter' ); ?></button>
+<button
+	class="btn-default"
+	<?php if ( ! is_admin() ) : ?>
+		@click="$refs.dialogRef.showModal(), openDialog = true"
+	<?php endif; ?>
+>
+	<?php esc_html_e( 'Open', 'wp-site-starter' ); ?>
+</button>
+</div>
