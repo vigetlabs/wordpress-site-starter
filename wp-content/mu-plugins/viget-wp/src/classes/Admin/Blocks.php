fb18dcdbf6a44c59fefb2897d9ffe3ba4611de46
Brian DiChiara <brian.dichiara@viget.com>
2024-11-11 10:35:00 -0700

    [N/A] Remove Template Part wrapper by default

--- wp-content/mu-plugins/viget-wp/src/classes/Admin/Blocks.php
+++ wp-content/mu-plugins/viget-wp/src/classes/Admin/Blocks.php
@@ -21,6 +21,9 @@ class Blocks {
 
 		// Remove the downloadable blocks directory feature.
 		$this->remove_downloadable_blocks();
+
+		// Remove template part wrapper div
+		$this->remove_template_part_wrapper();
 	}
 
 	/**
@@ -48,6 +51,49 @@ class Blocks {
 		remove_action(
 			'enqueue_block_editor_assets',
 			'wp_enqueue_editor_block_directory_assets'
+		);
+	}
+
+	/**
+	 * Remove the template part wrapper div
+	 *
+	 * @return void
+	 */
+	private function remove_template_part_wrapper(): void {
+		add_filter(
+			'render_block_core/template-part',
+			function( $block_content, $block ) {
+				$use_wrapper = $block['attrs']['useWrapper'] ?? false;
+
+				if ( true === $use_wrapper ) {
+					return $block_content;
+				}
+
+				$proc = new \WP_HTML_Tag_Processor( $block_content );
+
+				if ( true === $proc->next_tag() ) {
+					$block_content = trim( $block_content );
+
+					// Remove opening tag.
+					$block_content = substr(
+						$block_content,
+						strpos( $block_content, '>' ) + 1
+					);
+
+					// Remove closing tag.
+					$block_content = substr(
+						$block_content,
+						0,
+						strlen( $block_content ) - strlen( "</{$proc->get_tag()}>" ),
+					);
+
+					$block_content = trim( $block_content );
+				}
+
+				return $block_content;
+			},
+			10,
+			2
 		);
 	}
 }
