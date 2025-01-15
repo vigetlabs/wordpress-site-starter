ea3c96fb7a851d560675d9f1f429d3d07c6c98af
Brian DiChiara <brian.dichiara@viget.com>
2025-01-14 18:33:47 -0600

    [N/A] Added some pre-install/update composer scripts

--- bin/composer-scripts/ProjectEventHandler.php
+++ bin/composer-scripts/ProjectEventHandler.php
@@ -8,6 +8,7 @@ namespace Viget\ComposerScripts;
 use Composer\Script\Event;
 use Viget\ComposerScripts\ProjectEvents\PostCreateProjectScript;
 use Viget\ComposerScripts\ProjectEvents\PostInstallScript;
+use Viget\ComposerScripts\ProjectEvents\PreScripts;
 
 /**
  * Handle Project Events
@@ -34,5 +35,27 @@ class ProjectEventHandler {
 	 */
 	public static function postInstall( Event $event ): void {
 		PostInstallScript::init( $event );
+	}
+
+	/**
+	 * Pre install event.
+	 *
+	 * @param Event $event
+	 *
+	 * @return void
+	 */
+	public static function preInstall( Event $event ): void {
+		PreScripts::preInstall( $event );
+	}
+
+	/**
+	 * Pre update event.
+	 *
+	 * @param Event $event
+	 *
+	 * @return void
+	 */
+	public static function preUpdate( Event $event ): void {
+		PreScripts::preUpdate( $event );
 	}
 }
---
+++ bin/composer-scripts/ProjectEvents/PreScripts.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * Perform some pre-install/update checks with Composer.
+ */
+
+namespace Viget\ComposerScripts\ProjectEvents;
+
+use Composer\Script\Event;
+use Viget\ComposerScripts\ComposerScript;
+
+/**
+ * Pre Install/Update Composer Script
+ */
+class PreScripts extends ComposerScript {
+
+	/**
+	 * @var array
+	 */
+	private static array $repoPlugins = [
+		'viget-blocks-toolkit',
+		'viget-form-blocks',
+		'viget-parts-kit',
+	];
+
+	/**
+	 * Initialize the script.
+	 *
+	 * @param Event $event
+	 *
+	 * @return void
+	 */
+	public static function preInstall( Event $event ): void {
+		self::setEvent( $event );
+
+		// Do not run on deployment.
+		if ( ! $event->isDevMode() ) {
+			return;
+		}
+
+		// Check for repo plugins.
+		self::checkRepoPlugins();
+	}
+
+	/**
+	 * Initialize the script.
+	 *
+	 * @param Event $event
+	 *
+	 * @return void
+	 */
+	public static function preUpdate( Event $event ): void {
+		self::setEvent( $event );
+
+		// Do not run on deployment.
+		if ( ! $event->isDevMode() ) {
+			return;
+		}
+
+		// Check for repo plugins.
+		self::checkRepoPlugins();
+	}
+
+	/**
+	 * Check if repo plugins exist locally and skip their installation if they do.
+	 *
+	 * @return void
+	 */
+	private static function checkRepoPlugins(): void {
+		foreach ( self::$repoPlugins as $pluginName ) {
+			$pluginGitDir = self::translatePath( 'wp-content/plugins/' . $pluginName . '/.git' );
+			if ( is_dir( $pluginGitDir ) ) {
+				self::writeInfo( sprintf( 'Skipping installation of %s: directory containing repository exists.', $pluginName ) );
+
+				// Ensure the package is marked as "installed".
+				self::$event->getComposer()->getPackage()->setReplaces( array_merge(
+					self::$event->getComposer()->getPackage()->getReplaces(),
+					[
+						$pluginName => '*',
+					]
+				) );
+			}
+		}
+	}
+}
