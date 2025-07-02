bb3bfc7ba410289478b979ba161ae5df3f5b5229
Nathan Schmidt <91974372+nathan-schmidt-viget@users.noreply.github.com>
2025-07-02 09:45:45 -0600

    [#178] Updating to Tailwind 4

--- wp-content/themes/wp-starter/src/theme-json/styles/blocks/image.js
+++ wp-content/themes/wp-starter/src/theme-json/styles/blocks/image.js
@@ -1,9 +1,11 @@
+import theme from '../../import_tailwind.js';
+
 export default {
 	'core/image': {
 		variations: {
 			rounded: {
 				border: {
-					radius: 'var(--radius-default)',
+					radius: 'var(--radius-lg)',
 				},
 			},
 		},
