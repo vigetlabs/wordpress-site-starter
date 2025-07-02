61cc7b7bcfd4c18e0418c0007e6740a6886f26e6
Nathan Schmidt <91974372+nathan-schmidt-viget@users.noreply.github.com>
2025-07-02 07:55:20 -0600

    [#178] Revert TW upgrade

--- wp-content/themes/wp-starter/postcss.config.cjs
+++ wp-content/themes/wp-starter/postcss.config.cjs
@@ -2,8 +2,7 @@ module.exports = {
 	plugins: {
 		'postcss-import-ext-glob': {},
 		'postcss-import': {},
-		'tailwindcss/nesting': 'postcss-nesting',
-    	tailwindcss: {},
+		tailwindcss: {},
 		autoprefixer: {},
 		...(process.env.NODE_ENV === 'prod' ? { cssnano: {} } : {})
   },
