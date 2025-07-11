95a0506e41439ae5cd964eb71bc3701b8a56d38b
Nathan Schmidt <91974372+nathan-schmidt-viget@users.noreply.github.com>
2025-07-11 07:40:25 -0600

    [n/a] Fixing module bug with updated npm (#186)

--- wp-content/themes/wp-starter/src/theme-json/generate_theme.js
+++ wp-content/themes/wp-starter/src/theme-json/generate_theme.js
@@ -1,11 +1,11 @@
-import * as fs from 'fs';
+import fs from 'fs';
 import settings from './settings/_index.js';
 import styles from './styles/_index.js';
 import templateParts from './template-parts/_index.js';
 
 const CONTENT_PATH = 'src/theme-json';
 
-export default {
+const generateThemeJSON = {
 	name: 'generate-theme-json',
 	configureServer(server) {
 		// Watch the `CONTENT_PATH` directory for changes
@@ -27,4 +27,6 @@ function buildJSON() {
 
 	fs.writeFileSync('theme.json', JSON.stringify(data, null, 2));
 }
+
+export default generateThemeJSON;
 export { buildJSON };
