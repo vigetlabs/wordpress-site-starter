cc7a6f16b8a5f580e4b013d897b4cb505ca2e637
Brian DiChiara <122309362+bd-viget@users.noreply.github.com>
2026-04-21 09:02:51 -0500

    [#211] Allow colors in Tailwind but not Theme (#220)

--- wp-content/themes/wp-starter/src/theme-json/settings/color.js
+++ wp-content/themes/wp-starter/src/theme-json/settings/color.js
@@ -1,5 +1,9 @@
 import { getPalette, getGradients } from '../helpers/colors.js';
 
+/**
+ * Palette and gradients come from `src/styles/tailwind/inc/colors-palette.css` only.
+ * Add Tailwind-only tokens in `colors-utilities.css` so they stay out of the block editor.
+ */
 export default {
 	defaultDuotone: false,
 	defaultPalette: false,
