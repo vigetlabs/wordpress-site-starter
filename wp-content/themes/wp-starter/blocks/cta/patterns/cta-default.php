46f4c2f2927a92db10d797eb063b851c50d961bb
Brian DiChiara <brian.dichiara@viget.com>
2026-04-22 15:32:43 -0500

    [#216] WIP: Block Patterns

--- wp-content/themes/wp-starter/blocks/cta/patterns/cta-default.php
+++ wp-content/themes/wp-starter/blocks/cta/patterns/cta-default.php
@@ -8,16 +8,16 @@
  */
 
 ?>
-<!-- wp:heading {"placeholder":"Headline Goes Here","style":{"typography":{"textAlign":"center"}}} -->
-<h2 class="wp-block-heading has-text-align-center"></h2>
+<!-- wp:heading {"level":4,"placeholder":"Headline Goes Here","role":"content","style":{"typography":{"textAlign":"center"}}} -->
+<h4 class="wp-block-heading has-text-align-center"></h4>
 <!-- /wp:heading -->
 
-<!-- wp:paragraph {"placeholder":"Body text goes here...","style":{"typography":{"textAlign":"center"}}} -->
+<!-- wp:paragraph {"placeholder":"Body text goes here...","role":"content","style":{"typography":{"textAlign":"center"}}} -->
 <p class="has-text-align-center"></p>
 <!-- /wp:paragraph -->
 
-<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
-<div class="wp-block-buttons"><!-- wp:button {"placeholder":"Button Text"} -->
+<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"},"role":"content"} -->
+<div class="wp-block-buttons"><!-- wp:button {"placeholder":"Button Text","role":"content"} -->
 <div class="wp-block-button"><a class="wp-block-button__link wp-element-button"></a></div>
 <!-- /wp:button --></div>
 <!-- /wp:buttons -->
