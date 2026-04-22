dbbbd18b68796c3824e91f478c4c376f135ef9a9
Brian DiChiara <brian.dichiara@viget.com>
2026-04-22 11:15:50 -0500

    [#216] Current WIP attempt

--- wp-content/themes/wp-starter/blocks/cta/patterns/cta-default.php
+++ wp-content/themes/wp-starter/blocks/cta/patterns/cta-default.php
@@ -1,11 +1,23 @@
 <?php
 /**
- * Title: Call to Action (Full Width)
+ * Title: Call to Action (Default)
  * Slug: wp-starter/cta-default
  * Categories: cta
+ * Inserter: false
  * Viewport width: 1400
  */
 
 ?>
-<!-- wp:acf/cta {"name":"acf/cta","data":{},"mode":"preview","layout":{"type":"constrained"}} -->
-<!-- /wp:acf/cta -->
+<!-- wp:heading {"placeholder":"Headline Goes Here","style":{"typography":{"textAlign":"center"}}} -->
+<h2 class="wp-block-heading has-text-align-center"></h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"placeholder":"Body text goes here...","style":{"typography":{"textAlign":"center"}}} -->
+<p class="has-text-align-center"></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
+<div class="wp-block-buttons"><!-- wp:button {"placeholder":"Button Text"} -->
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button"></a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons -->
